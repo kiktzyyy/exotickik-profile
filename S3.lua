ef177d0a651443a41e52c22c3610cc2bc129781d
kiktzyyy <ppppbokik@gmail.com>
2025-07-27 07:20:36 +0700

    Create S3.lua

---
+++ S3.lua
@@ -0,0 +1,40 @@
+    spawn(function()
+		while wait(0.2) do
+			if _G.Settings.Items["Auto Third Sea"] then
+				pcall(function()
+					if (game:GetService("Players")).LocalPlayer.Data.Level.Value >= 1500 and World2 then
+						_G.Settings.Main["Auto Farm"] = false;
+						if (game:GetService("ReplicatedStorage")).Remotes.CommF_:InvokeServer("ZQuestProgress", "General") == 0 then
+							topos(CFrame.new(-1926.3221435547, 12.819851875305, 1738.3092041016));
+							if ((CFrame.new((-1926.3221435547), 12.819851875305, 1738.3092041016)).Position - (game:GetService("Players")).LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 10 then
+								wait(1.5);
+								(game:GetService("ReplicatedStorage")).Remotes.CommF_:InvokeServer("ZQuestProgress", "Begin");
+							end;
+							wait(1.8);
+							if (game:GetService("Workspace")).Enemies:FindFirstChild("rip_indra") then
+								for i, v in pairs((game:GetService("Workspace")).Enemies:GetChildren()) do
+									if v.Name == "rip_indra" then
+										OldCFrameThird = v.HumanoidRootPart.CFrame;
+										repeat
+											(game:GetService("RunService")).Heartbeat:wait();
+											AutoHaki();
+											EquipWeapon(_G.Settings.Main["Selected Weapon"]);
+											topos(v.HumanoidRootPart.CFrame * Pos);
+											v.HumanoidRootPart.CFrame = OldCFrameThird;
+											v.HumanoidRootPart.Size = Vector3.new(1, 1, 1);
+											v.HumanoidRootPart.CanCollide = false;
+											v.Humanoid.WalkSpeed = 0;
+											Attack();
+											(game:GetService("ReplicatedStorage")).Remotes.CommF_:InvokeServer("TravelZou");
+										until _G.Settings.Items["Auto Third Sea"] == false or v.Humanoid.Health <= 0 or (not v.Parent);
+									end;
+								end;
+							elseif not (game:GetService("Workspace")).Enemies:FindFirstChild("rip_indra") and ((CFrame.new((-26880.93359375), 22.848554611206, 473.18951416016)).Position - (game:GetService("Players")).LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 1000 then
+								topos(CFrame.new(-26880.93359375, 22.848554611206, 473.18951416016));
+							end;
+						end;
+					end;
+				end);
+			end;
+		end;
+	end);
