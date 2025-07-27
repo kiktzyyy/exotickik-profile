72b1cb05d40d74197720b1a2e211ab2905459e5e
kiktzyyy <ppppbokik@gmail.com>
2025-07-27 07:28:42 +0700

    Create Attacksea.lua

---
+++ Attacksea.lua
@@ -0,0 +1,79 @@
+local gg = getrawmetatable(game);
+local old = gg.__namecall;
+setreadonly(gg, false);
+gg.__namecall = newcclosure(function(...)
+	local method = getnamecallmethod();
+	local args = {
+		...
+	};
+	if tostring(method) == "FireServer" then
+		if tostring(args[1]) == "RemoteEvent" then
+			if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
+				if Skillaimbot then
+					args[2] = AimBotSkillPosition;
+					return old(unpack(args));
+				end;
+			end;
+		end;
+	end;
+	return old(...);
+end);
+spawn(function()
+	while wait(0.2) do
+		pcall(function()
+			if UseSkill then
+				for i, v in pairs((game:GetService("Workspace")).Enemies:GetChildren()) do
+					if v.Name == MonFarm and v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health <= v.Humanoid.MaxHealth * _G.Settings.Setting["Mastery Health"] / 100 then
+						if _G.Settings.Setting["Fruit Mastery Skill Z"] then
+							(game:service("VirtualInputManager")):SendKeyEvent(true, "Z", false, game);
+							wait(_G.Settings.Hold["Hold Mastery Skill Z"]);
+							(game:service("VirtualInputManager")):SendKeyEvent(false, "Z", false, game);
+						end;
+						if _G.Settings.Setting["Fruit Mastery Skill X"] then
+							(game:service("VirtualInputManager")):SendKeyEvent(true, "X", false, game);
+							wait(_G.Settings.Hold["Hold Mastery Skill X"]);
+							(game:service("VirtualInputManager")):SendKeyEvent(false, "X", false, game);
+						end;
+						if _G.Settings.Setting["Fruit Mastery Skill C"] then
+							(game:service("VirtualInputManager")):SendKeyEvent(true, "C", false, game);
+							wait(_G.Settings.Hold["Hold Mastery Skill C"]);
+							(game:service("VirtualInputManager")):SendKeyEvent(false, "C", false, game);
+						end;
+						if _G.Settings.Setting["Fruit Mastery Skill V"] then
+							(game:service("VirtualInputManager")):SendKeyEvent(true, "V", false, game);
+							wait(_G.Settings.Hold["Hold Mastery Skill V"]);
+							(game:service("VirtualInputManager")):SendKeyEvent(false, "V", false, game);
+						end;
+						if _G.Settings.Setting["Fruit Mastery Skill F"] then
+							(game:service("VirtualInputManager")):SendKeyEvent(true, "F", false, game);
+							wait(_G.Settings.Hold["Hold Mastery Skill F"]);
+							(game:service("VirtualInputManager")):SendKeyEvent(false, "F", false, game);
+						end;
+					end;
+				end;
+			end;
+		end);
+	end;
+end);
+spawn(function()
+	while wait() do
+		pcall(function()
+			if UseGunSkill then
+				for i, v in pairs((game:GetService("Workspace")).Enemies:GetChildren()) do
+					if v.Name == MonFarm and v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health <= v.Humanoid.MaxHealth * _G.Settings.Setting["Mastery Health"] / 100 then
+						if _G.Settings.Setting["Gun Mastery Skill Z"] then
+							(game:service("VirtualInputManager")):SendKeyEvent(true, "Z", false, game);
+							wait(0.5);
+							(game:service("VirtualInputManager")):SendKeyEvent(false, "Z", false, game);
+						end;
+						if _G.Settings.Setting["Gun Mastery Skill X"] then
+							(game:service("VirtualInputManager")):SendKeyEvent(true, "X", false, game);
+							wait(0.5);
+							(game:service("VirtualInputManager")):SendKeyEvent(false, "X", false, game);
+						end;
+					end;
+				end;
+			end;
+		end);
+	end;
+end);
